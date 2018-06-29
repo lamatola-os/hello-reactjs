913e1cd61b2872c5f8b25a6c9ce16e4b4d7dadbb
Upadhyay <prayag.upd@gmail.com>
2018-06-29 00:24:47 -0700

    understand reacjs state/ behaviour concepts

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -7,4 +7,48 @@ class Introgen extends React.Component {
   }
 }
 
-render(<Introgen/>, document.getElementById('introgen'));
+class Thoughts extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {name: '', feeling: ''};
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleFeelingChange = this.handleFeelingChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleFeelingChange(event) {
+    this.setState({feeling: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.name + ': ' + this.state.feeling);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+
+        <label>
+          Name:
+          <input type="text" value={this.state.name} onChange={this.handleNameChange} />
+        </label>
+
+        <label>
+          How you feeling?:
+          <input type="text" value={this.state.feeling} onChange={this.handleFeelingChange} />
+        </label>
+
+        <input type="submit" value="Share" />
+      </form>
+    );
+  }
+}
+
+render(<Thoughts/>, document.getElementById('introgen'));
